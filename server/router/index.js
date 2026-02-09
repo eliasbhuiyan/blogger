06951bba7bc6df05462e655abf9ac6d94b161d44
unknown <eliasbhuiyan21@gmail.com>
2026-02-09 21:03:11 +0600

    vercel config

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use("/", (req, res) => {
+    res.redirect(process.env.CLIENT_URL)
+})
+
 router.use("/auth", require("./auth"));
 
 router.use("/blog", require("./blog"));
