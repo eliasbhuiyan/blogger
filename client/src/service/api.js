e6be877ae809f93df95152d186606d0083724386
unknown <eliasbhuiyan21@gmail.com>
2026-02-09 21:26:23 +0600

    vercel config

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -2,42 +2,42 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://blogger-server-site.vercel.app/", credentials: "include" }),
-  tagTypes: ['User', "Blog"],
+  baseQuery: fetchBaseQuery({ baseUrl: "https://blogger-server-site.vercel.app", credentials: "include" }),
+  tagTypes: ["Blog"],
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: "auth/login",
+        url: "/auth/login",
         method: "POST",
         body: data,
       }),
     }),
     getUser: builder.query({
-      query: () => "auth/profile",
+      query: () => "/auth/profile",
     }),
     refreshToken: builder.mutation({
       query: () => ({
-        url: "auth/refresh-token",
+        url: "/auth/refresh-token",
         method: "POST",
       }),
     }),
     getBlogList: builder.query({
-      query: () => "blog/list",
+      query: () => "/blog/list",
     }),
     getBlogListByUser: builder.query({
-      query: () => "blog/list-by-user",
+      query: () => "/blog/list-by-user",
       providesTags: ["Blog"]
     }),
     createBlog: builder.mutation({
       query: (data) => ({
-        url: "blog/upload",
+        url: "/blog/upload",
         method: "POST",
         body: data,
       }),
       invalidatesTags: ["Blog"]
     }),
     blogDetails: builder.query({
-      query: (slug) => `blog/read/${slug}`,
+      query: (slug) => `/blog/read/${slug}`,
     }),
   }),
 });
--- server/index.js
+++ server/index.js
@@ -5,13 +5,11 @@ const router = require("./router");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 app.use(express.json());
-console.log(process.env.CLIENT_URL);
 
 app.use(cors(
   {
     origin: 'https://blogger-client-site.vercel.app',
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-    credentials: true // Set to true if your app uses cookies or auth tokens
+    credentials: true
   }
 ));
 const dbConfig = require("./dbconfig");
